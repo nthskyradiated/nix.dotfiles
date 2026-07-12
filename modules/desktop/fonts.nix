a6aa46ca5496d3d5f1b7a900c3ec6e525633f6d8
andy <andy.pandaan@outlook.com>
2026-07-12 22:40:46 +0400

    nvim: modified nvim ui

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -5,13 +5,14 @@
     nerd-fonts.fira-code
     nerd-fonts.caskaydia-mono
     adwaita-fonts
+    iosevka
   ];
 
   fonts.fontconfig = {
     defaultFonts = {
       serif = [ "Liberation Serif" ];
       sansSerif = [ "FiraCode Nerd Font Mono" ];
-      monospace = [ "JetBrains Mono" ];
+      monospace = [ "iosevka" ];
     };
   };
 }
