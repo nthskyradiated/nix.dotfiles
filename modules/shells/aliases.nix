cdad905c6a44dbef999a31bbd5c0072ade8a93b2
andy <andy.pandaan@outlook.com>
2026-07-25 20:32:11 +0400

    updated flake.nix

--- modules/shells/aliases.nix
+++ modules/shells/aliases.nix
@@ -4,7 +4,7 @@
   ls = "eza -l --icons";
   k = "kubectl";
   nrs = "sudo nixos-rebuild switch --flake ~/nix.dotfiles#${hostname}";
-  nixdelgrub = "sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system && sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch";
+  nixdelgrub = "sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system && sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch && sudo nix-store --gc";
 
   ovpn-start = "openvpn3 session-start --config ~/.config/openvpn/work.ovpn";
   ovpnls = "openvpn3 sessions-list";
